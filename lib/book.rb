886374e77a0fb902b89494a96c4e12b74b0b9506
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-31 22:25:51 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,18 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
+  GENRRES = []
+  
   def initialize(title)
     @title = title
   end
 
+  def genre= (genre)
+    @genre = genre
+    GENRES = GENRES << genre .uniq
+  end
+  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
